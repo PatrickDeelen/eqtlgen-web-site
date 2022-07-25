915c8a1d6c7042fe692842d6c08efe1d4c8436c7
Robert Warmerdam <robert_warmerdam@hotmail.com>
2022-07-25 17:03:15 +0200

    Updated workflow to incorporate action

--- .github/actions/render-rmd-action/render-rmarkdown.R
+++ .github/actions/render-rmd-action/render-rmarkdown.R
@@ -1,2 +1,8 @@
 library(rmarkdown)
+library(devtools)
+
+if (!require("emo",character.only = TRUE)) {
+    devtools::install_github("hadley/emo")
+}
+
 for (rmd_file in list.files(pattern = "/*.Rmd")) {render(input = rmd_file, output_format = md_document(preserve_yaml = TRUE))}
